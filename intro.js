84e46330a38df3132c1128884fe78bfe2a277804
Michael Ruvinshteyn <mruvinshteyn@stuy.edu>
2017-12-08 18:38:47 -0500

    finished functions

--- intro.js
+++ intro.js
@@ -5,18 +5,55 @@ HW#16: Sequential Progression
 2017-12-08  
 */
 
+//current last element number
+var currElem = 7;
+
+//adds element to list upon button-press
 var addElement = function(){
+    currElem += 1;
     var elem = document.createElement("li");
-    elem.appendChild(document.createTextNode("an element"));
+    elem.innerHTML = "item " + currElem;
     document.getElementById("thelist").appendChild(elem);
-    console.log("heres an element");
+    run(elem); //activates other functions on inserted element
+    console.log(currElem + " items total");
 };
 
+//button used to add element to list
 var button = document.getElementById("b");
 b.addEventListener('click', addElement);
 
+//changes text of heading after hovering over item in the list
 var changeHeading = function(){
-    console.log ("wassup");
+    var h = document.getElementById('h');
+    h.innerHTML = this.innerHTML;
 };
-var hover = document.getElementById("thelist");
-thelist.addEventListener('mouseover', changeHeading);
+
+//changes text of heading back to normal after mousing away from item in the list
+var changeHeadingBack = function(){
+    var h = document.getElementById('h');
+    h.innerHTML = 'Hello World';
+};
+
+//removes item from list upon mouse-click
+var removeItem = function(){
+    currElem -= 1;
+    this.remove();
+    var lis = document.getElementsByTagName('li');
+    //sets item names to correspond to their numbers
+    for (li = 0; li < lis.length; li++){
+        lis[li].innerHTML = "item " + li;
+    }
+};
+
+//function to activate changeHeading, changeHeadingBack, and removeItem for an item
+var run = function(item){
+    item.addEventListener('mouseover', changeHeading);
+    item.addEventListener('mouseout', changeHeadingBack);
+    item.addEventListener('click', removeItem);
+};
+
+//creates an array of all elements in the list
+var lis = document.getElementsByTagName('li');
+for (li = 0; li < lis.length; li++){
+    run(lis[li]); //activates each item in the list
+};
