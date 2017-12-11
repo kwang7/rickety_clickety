768fc9bb3765701d0d8ff927dd51e82fb412d1c7
kwang7 <kwang7@stuy.edu>
2017-12-11 12:11:13 -0500

    :poop: :poop: :poop:

--- intro.js
+++ intro.js
@@ -7,6 +7,8 @@ HW#16: Sequential Progression
 
 //current last element number
 var currElem = 7;
+var currElem2 = 0;
+var currElem3 = 1;
 
 //adds element to list upon button-press
 var addElement = function(){
@@ -21,6 +23,47 @@ var addElement = function(){
 //button used to add element to list
 var button = document.getElementById("b");
 b.addEventListener('click', addElement);
+
+//fxn to find the nth fibonacci number
+var fibonacci = function (n){
+    if (n == 0)
+	return 0;
+    if (n == 1 || n == 2)
+	return 1;
+    else
+	return fibonacci(n-1) + fibonacci(n-2);
+};
+
+///adds nth fibonacci number upon button-press
+var add_fib = function(){
+    var elem = document.createElement("li");
+    elem.innerHTML =  currElem2 +"th fibonacci number: " + fibonacci(currElem2);
+    document.getElementById("thesecondlist").appendChild(elem);
+    run(elem); //activates other functions on inserted element
+    console.log(currElem2 + " items total");
+    currElem2 += 1;
+
+};
+
+var buttun = document.getElementById("b2");
+b2.addEventListener('click', add_fib);
+
+//adds nth triangular number to list upon button-press
+var add_triangle = function(){
+    var elem = document.createElement("li");
+    elem.innerHTML =  currElem3-1 +"th triange number: " + triangle(currElem3);
+    document.getElementById("thethirdlist").appendChild(elem);
+    run(elem); //activates other functions on inserted element
+    console.log(currElem3 + " items total");
+    currElem3 += 1;
+};
+
+var triangle = function( n ){
+    return ( ( n * (n - 1 ))/2 );
+}
+
+var bottun = document.getElementById("b3");
+b3.addEventListener('click', add_triangle);
 
 //changes text of heading after hovering over item in the list
 var changeHeading = function(){
@@ -40,11 +83,13 @@ var changeHeadingBack = function(){
 var removeElement = function(){
     currElem -= 1;
     this.remove();
+    /*
     var lis = document.getElementsByTagName('li');
     //sets item names to correspond to their numbers
     for (li = 0; li < lis.length; li++){
         lis[li].innerHTML = "item " + li;
     }
+    */
     console.log(currElem + " items total");
 };
 
